6993a39c46533d323492a829ea0983d54d26a438
sidra-prof <chaudharysidra7708@gmail.com>
2026-07-11 16:30:46 +0530

    Implement Array Palindrome using Two Pointers

---
+++ Arrays/09-Palindrome.cpp
@@ -0,0 +1,43 @@
+/*
+----------------------------------------
+Problem : Check if the Array is Palindrome or not
+Difficulty : Easy
+Approach : Linear Traversal
+Time Complexity : O(n)
+Space Complexity : O(1)
+Author : Sidra
+Date : 11-07-2026
+----------------------------------------
+*/
+#include<iostream>
+#include<vector>
+
+using namespace std;
+bool Palindrome(vector<int>& arr);
+int main(){
+    int size;
+    cout<<"Enter the size of an Array: \n";
+    cin>>size;
+    vector<int>array(size);
+    cout<<"Enter the Elements of an Array:\n";
+    for(int i=0;i<array.size();i++){
+        cin>>array[i];
+    }
+    // Check palindrome
+    if(Palindrome(array)){
+        cout<<"Palindrome\n";
+    }else{
+        cout<<"Not a Palindrome\n";
+    }
+    return 0;
+}
+
+bool Palindrome(vector<int>&arr){
+    for(int i=0,j=arr.size();i<j;i++,j--){
+        if(arr[i]!=arr[j]){
+            return false;
+        }
+    }
+    return true;
+
+}
